e5a9aba1dc53cf930dfe36d167e4d0bd266b2c0b
薛子豪 <42602346+024026008@users.noreply.github.com>
2020-07-13 20:32:34 +0800

    Update 文字加密解密-合并版.py
    v1.1

--- 文字加密解密-合并版.py
+++ 文字加密解密-合并版.py
@@ -30,19 +30,20 @@ def dc(p,pw_1,pw_2,pw_3,pw_4,pw_5):
                             break
                         index = (g << 8) + b
                         lst.append(chr(index-pw_5))
-                return ''.join(lst)
+                re = ''.join(lst)
             else:
-                pass
+                re = str('密码错误，解密失败！')
         else:
-            pass
+            re = str('密码错误，解密失败！')
     else:
-        pass
+        re = str('密码错误，解密失败！')
+    return re
 if __name__ == '__main__':
     print('按照提示输入数据，请勿作死！如果程序闪退，可能是程序出错。请尝试重新运行或联系作者。by：024026008 可以在bilibili联系我。')
     print('原作者（GitHub）：3150601355')
     print('BUG?不存在的！（手动滑稽）\n')
     while True:
-        choose = str(input('1：加密；2：解密；3：退出。输入数字选择：'))
+        choose = str(input('\n1：加密；2：解密；3：退出。输入数字选择：'))
         if choose == '1':
             filename1 = input('请输入文件位置：')
             Pw_1 = int(input('请输入随机数作为加密密码（0~255）[01]：'))
@@ -52,17 +53,18 @@ if __name__ == '__main__':
             Pw_5 = int(input('请输入随机数作为数据位移码（0~3）：'))
             with open(filename1,encoding="UTF-8") as f1:
                 whole_text1 = f1.read()
-            im = ec(whole_text,Pw_1,Pw_2,Pw_3,Pw_4,Pw_5)
+            im = ec(whole_text1,Pw_1,Pw_2,Pw_3,Pw_4,Pw_5)
             im.save("output.bmp")
             passwd = str(hex(Pw_1)) +' '+ str(hex(Pw_2)) +' '+ str(hex(Pw_3)) +' '+ str(hex(Pw_4)) +' '+ str(bin(Pw_5))
-            print('文件已保存。解密密码（空格不能少！）：' + passwd)
+            print('文件已保存至应用目录下的“output.bmp”。解密密码（空格不能少！）：' + passwd)
         elif choose == '2':
             filename2 = input('请输入文件位置：')
-            passwd = input('请输入密码：')
+            passwd = input('请输入密码(格式：aaaa bbbb cccc dddd eeee ffff)：')
             pwlist = passwd.split(' ')
             whole_text2 = dc(Image.open(filename2,"r"),int(pwlist[0],16),int(pwlist[1],16),int(pwlist[2],16),int(pwlist[3],16),int(pwlist[4],2))
             with open("output.txt","w",encoding = "UTF-8") as f2:
                 f2.write(whole_text2)
+            print('文件已保存至应用目录下的“output.txt”。')
         elif choose == '3':
             break
         else:
